09bc952abc7550fc8cf8695db6b36af7610ae68c
sofwarearihan@gmail.com <sofwarearihan@gmail.com>
2024-11-27 09:34:04 -0500

    code is good but motor not working, pwm issues

--- RCBot_Receiver/Core/Src/main.c
+++ RCBot_Receiver/Core/Src/main.c
@@ -5,6 +5,8 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
+#include <string.h>
+#include <stdio.h>
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -15,6 +17,9 @@
 /* USER CODE BEGIN PD */
 #define UART_TIMEOUT 100   // Timeout for UART reception in ms
 #define MESSAGE_LENGTH 6   // Length of "XXXX\r\n" where XXXX is the ADC value
+#define MOTOR_FORWARD 0
+#define MOTOR_BACKWARD 1
+#define MOTOR_STOP 2
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -22,6 +27,7 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+TIM_HandleTypeDef htim1;
 TIM_HandleTypeDef htim2;
 
 UART_HandleTypeDef huart1;
@@ -38,11 +44,51 @@ static void MX_GPIO_Init(void);
 static void MX_USART1_UART_Init(void);
 static void MX_USART2_UART_Init(void);
 static void MX_TIM2_Init(void);
+static void MX_TIM1_Init(void);
+
+void setMotorDirection(uint8_t direction);
+void setMotorSpeed(uint16_t speed);
+
 /* USER CODE BEGIN PFP */
+#define AIN1_PIN GPIO_PIN_4
+#define AIN1_PORT GPIOB
+#define AIN2_PIN GPIO_PIN_5
+#define AIN2_PORT GPIOB
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+// Set motor direction using AIN1 and AIN2 pins
+void setMotorDirection(uint8_t direction)
+{
+    switch(direction) {
+        case MOTOR_FORWARD:
+            HAL_GPIO_WritePin(AIN1_PORT, AIN1_PIN, GPIO_PIN_SET);
+            HAL_GPIO_WritePin(AIN2_PORT, AIN2_PIN, GPIO_PIN_RESET);
+            break;
+        case MOTOR_BACKWARD:
+            HAL_GPIO_WritePin(AIN1_PORT, AIN1_PIN, GPIO_PIN_RESET);
+            HAL_GPIO_WritePin(AIN2_PORT, AIN2_PIN, GPIO_PIN_SET);
+            break;
+        case MOTOR_STOP:
+            HAL_GPIO_WritePin(AIN1_PORT, AIN1_PIN, GPIO_PIN_RESET);
+            HAL_GPIO_WritePin(AIN2_PORT, AIN2_PIN, GPIO_PIN_RESET);
+            break;
+    }
+}
+
+// Set motor speed using PWM
+void setMotorSpeed(uint16_t speed)
+{
+    // Ensure speed is within bounds (0-1000)
+    if(speed > 1000) speed = 1000;
+    __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, speed);
+}
+
+// Utility function to map values from one range to another
+int32_t map(int32_t x, int32_t in_min, int32_t in_max, int32_t out_min, int32_t out_max) {
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
 /* USER CODE END 0 */
 
 /**
@@ -74,11 +120,18 @@ int main(void)
   MX_USART1_UART_Init();
   MX_USART2_UART_Init();
   MX_TIM2_Init();
+  MX_TIM1_Init();
   /* USER CODE BEGIN 2 */
   HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
 
+  HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
   HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_2);
   __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_2, 750);
+
+  __HAL_TIM_MOE_ENABLE(&htim1);  // Enable main output
+
+  setMotorDirection(MOTOR_STOP);
+  setMotorSpeed(0);
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -88,24 +141,38 @@ int main(void)
 
     /* USER CODE BEGIN 3 */
 	// Try to receive the complete message
-	if (HAL_UART_Receive(&huart1, message, MESSAGE_LENGTH, UART_TIMEOUT) == HAL_OK)
-	{
-	  // Toggle LED when full message received
-	  HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+	  if (HAL_UART_Receive(&huart1, message, MESSAGE_LENGTH, UART_TIMEOUT) == HAL_OK)
+	  {
+		  message[4] = '\0';
+		  received_value = atoi((char*)message);
 
-	  // Null terminate the string for safe conversion
-	  message[4] = '\0';
+		  // Debug message
+		  char debug_msg[50];
 
-	  // Convert string to number
-	  received_value = atoi((char*)message);
+		  if(received_value < 1800) {
+			  setMotorDirection(MOTOR_BACKWARD);
+			  uint16_t speed = map(received_value, 0, 1800, 1000, 0);
+			  setMotorSpeed(speed);
+			  sprintf(debug_msg, "BWD: ADC=%d, Speed=%d\r\n", received_value, speed);
+		  }
+		  else if(received_value > 2200) {
+			  setMotorDirection(MOTOR_FORWARD);
+			  uint16_t speed = map(received_value, 2200, 4095, 0, 1000);
+			  setMotorSpeed(speed);
+			  sprintf(debug_msg, "FWD: ADC=%d, Speed=%d\r\n", received_value, speed);
+		  }
+		  else {
+			  setMotorDirection(MOTOR_STOP);
+			  setMotorSpeed(0);
+			  sprintf(debug_msg, "STOP: ADC=%d\r\n", received_value);
+		  }
 
-	  double horizontalRatio = 2 * ((double)received_value / 4096) - 1;
-	  double rotationValue = 750 + 500*horizontalRatio;
-	  __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_2, rotationValue);
+		  char debug__msg[50];
+		  sprintf(debug__msg, "Timer Period: %lu\r\n", htim1.Init.Period);
+		  HAL_UART_Transmit(&huart2, (uint8_t*)debug__msg, strlen(debug__msg), UART_TIMEOUT);
 
-	  // Optional: Echo received value to UART2 for debug
-	  HAL_UART_Transmit(&huart2, message, MESSAGE_LENGTH, UART_TIMEOUT);
-	}
+		  HAL_UART_Transmit(&huart2, (uint8_t*)debug_msg, strlen(debug_msg), UART_TIMEOUT);
+	  }
   }
   /* USER CODE END 3 */
 }
@@ -154,6 +221,71 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+}
+
+/**
+  * @brief TIM1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM1_Init(void)
+{
+
+  /* USER CODE BEGIN TIM1_Init 0 */
+
+  /* USER CODE END TIM1_Init 0 */
+
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+  TIM_OC_InitTypeDef sConfigOC = {0};
+  TIM_BreakDeadTimeConfigTypeDef sBreakDeadTimeConfig = {0};
+
+  /* USER CODE BEGIN TIM1_Init 1 */
+
+  /* USER CODE END TIM1_Init 1 */
+  htim1.Instance = TIM1;
+  htim1.Init.Prescaler = 83;
+  htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim1.Init.Period = 999;
+  htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim1.Init.RepetitionCounter = 0;
+  htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
+  if (HAL_TIM_PWM_Init(&htim1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim1, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sConfigOC.OCMode = TIM_OCMODE_PWM1;
+  sConfigOC.Pulse = 0;
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+  sConfigOC.OCNPolarity = TIM_OCNPOLARITY_HIGH;
+  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+  sConfigOC.OCIdleState = TIM_OCIDLESTATE_RESET;
+  sConfigOC.OCNIdleState = TIM_OCNIDLESTATE_RESET;
+  if (HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sBreakDeadTimeConfig.OffStateRunMode = TIM_OSSR_DISABLE;
+  sBreakDeadTimeConfig.OffStateIDLEMode = TIM_OSSI_DISABLE;
+  sBreakDeadTimeConfig.LockLevel = TIM_LOCKLEVEL_OFF;
+  sBreakDeadTimeConfig.DeadTime = 0;
+  sBreakDeadTimeConfig.BreakState = TIM_BREAK_DISABLE;
+  sBreakDeadTimeConfig.BreakPolarity = TIM_BREAKPOLARITY_HIGH;
+  sBreakDeadTimeConfig.AutomaticOutput = TIM_AUTOMATICOUTPUT_DISABLE;
+  if (HAL_TIMEx_ConfigBreakDeadTime(&htim1, &sBreakDeadTimeConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM1_Init 2 */
+
+  /* USER CODE END TIM1_Init 2 */
+  HAL_TIM_MspPostInit(&htim1);
+
 }
 
 /**
@@ -309,14 +441,6 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-
-  /*Configure GPIO pin : PA8 */
-  GPIO_InitStruct.Pin = GPIO_PIN_8;
-  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  GPIO_InitStruct.Alternate = GPIO_AF1_TIM1;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   /*Configure GPIO pins : PB4 PB5 PB8 */
